dd6c0f62274dcd1462714a99efb89629ccf367e9
kznrluk <kznr.luk@gmail.com>
2019-04-21 20:49:08 +0900

    refactor

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # portal-calender
-富士通製大学ポータルから履修情報を取得し、Googleカレンダーが読み込める形式で出力します。
-
+富士通製大学ポータルから履修情報を取得し、Googleカレンダーが読み込める形式(CSV)で出力します。
 
 `node.js`のインストールが必要です。
 ```
@@ -8,4 +7,25 @@ npm install
 node exportToGoogleCalendar.js
 ```
 
-使用前にソースコード内のUSER_DATAを編集してください。
+### config.json
+```
+{
+  // ポータルログインに指定するユーザID
+  "userId": "N1732000",
+  // パスワード
+  "password": "passwordHere",
+  // カレンダーを作成する開始日(デフォルトでコマンド実行日)
+  "createCalendarFrom": "2019-04-01",
+  // カレンダーを作成する終了日(デフォルトでコマンド実行日+一週間)
+  "createCalendarTo": "2019-08-01",
+  // 出力ファイル名
+  "fileName": "./export.csv"
+}
+```
+
+`createCalendarFrom`と`createCalendarTo`を入力することで指定された期間の間繰り返しで予定を作成します。
+
+これはGoogleカレンダー上で指定する繰り返しとは違い、単純に予定を指定日数分生成する処理を行うものです。そのため、Googleカレンダー上での一括編集には対応していません。
+
+CSVをインポートする際は、他の予定が入っていない新しいカレンダーを作成してから行うのがおすすめです。
+
--- exportToGoogleCalendar.js
+++ exportToGoogleCalendar.js
@@ -1,21 +1,11 @@
+const fs = require('fs');
+const SETTING = require('./config');
 const getCalenderData = require('./lib/getCalenderData');
 const convertArrayToCSV = require('./lib/converArrayToCSV');
-const fs = require('fs');
-
-const USER_DATA = {
-    id: '',
-    pass: '',
-};
-
-const CONFIG = {
-    start: '2019-04-19',
-    end: '2019-07-25',
-};
-
-const fileName = 'export.csv';
 
 (async () => {
-    const calenderData = await getCalenderData(USER_DATA);
-    const csv = convertArrayToCSV(calenderData, CONFIG.start, CONFIG.end);
-    fs.writeFileSync(fileName, csv);
+    const calenderData = await getCalenderData(SETTING.userId, SETTING.password);
+    const csv = convertArrayToCSV(calenderData, SETTING.createCalendarFrom, SETTING.createCalendarTo);
+    fs.writeFileSync(SETTING.fileName, csv);
+    console.log('\u001b[36m', 'Done.', '\u001b[0m');
 })();
